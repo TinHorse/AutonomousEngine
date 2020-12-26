7c92a76ab33b6af28b2cdabc444b38ae26f62221
Lars Brestrich <l.brestrich@protonmail.com>
2020-12-26 14:38:07 +0000

    Fixed game loop

--- main.cpp
+++ main.cpp
@@ -7,26 +7,26 @@ int main(int argc,  char *argv[])
 	const int frameDelay = 1000 / FPS; // sets frameDelay = (milliseconds in a second / FPS)
 	// this means that there should be a new frame approx. every 13 milliseconds
 
-	Uint32 frameStart;
-	int frameTime;
+	double prevTime = SDL_GetTicks();
+	double lag = 0.0f;
 
 	game = new Game();
 	game->Init("BoidEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 640, false);
 
 	while (game->Running())
 	{
-		frameStart = SDL_GetTicks(); // get total ticks (milliseconds) since initialization
+		double currTime = SDL_GetTicks();
+		double elapsed = currTime - prevTime;
+		prevTime = currTime;
+		lag += elapsed;
 
 		game->HandleEvents();
-		game->Update();
-		game->Render();
-
-		frameTime = SDL_GetTicks() - frameStart; // get ticks passed since last tick by subtracting frameStart ticks from current ticks
-		if (frameTime < frameDelay) // if ticks passed since last tick are smaller than 13 milliseconds
+		while (lag >= frameDelay)
 		{
-			SDL_Delay(frameDelay - frameTime); // delay the next frame by 13 - frameTime
+			game->Update();
+			lag -= frameDelay;
 		}
-		std::cout << frameDelay - frameTime << std::endl;
+		game->Render();
 	}
 	game->Clean(); // delete game memory
 
